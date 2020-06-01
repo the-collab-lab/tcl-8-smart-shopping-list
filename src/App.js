725b5c8f13789630e96077790564b026a49d2b63
abidishajia <abidishajia@gmail.com>
2020-06-01 17:30:55 -0500

    added react router functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import AddItem from './components/AddItem';
+import List from './components/List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="nav">
+        <div>
+          <Link to="/">List</Link>
+        </div>
+        <div>
+          <Link to="/addItem">Add Item</Link>
+        </div>
+      </div>
+      <Route exact path="/" component={List} />
+      <Route path="/addItem" component={AddItem} />
     </div>
   );
 }
