ffdd01efb8108473d25e329e7d481a51234ffd10
AliLynne <alilynne.thompson@gmail.com>
2020-06-14 13:12:36 -0700

    move token logic to app component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,31 +1,6 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 
-import GetToken from '../lib/token.js';
-
-const Welcome = () => {
-  let history = useHistory();
-  const push = history.push;
-
-  const handleClick = () => {
-    const token = GetToken();
-    saveToken(token);
-    push('/list');
-  };
-
-  const checkForToken = () => {
-    return localStorage.getItem('userToken') ? true : false;
-  };
-
-  const saveToken = token => {
-    localStorage.setItem('userToken', token);
-    console.log('token saved!');
-  };
-
-  useEffect(() => {
-    checkForToken() ? push('/list') : push('/');
-  }, [push]);
-
+const Welcome = ({ handleClick }) => {
   return (
     <div>
       <h2>Welcome to ShoppingList</h2>
