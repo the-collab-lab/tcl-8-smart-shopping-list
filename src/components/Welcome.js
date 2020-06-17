02d829738126d9dde449cdab9fedc24a4fdd5ac2
AliLynne <alilynne.thompson@gmail.com>
2020-06-17 11:05:23 -0700

    add join list field to welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,6 +5,11 @@ const Welcome = ({ handleClick }) => {
     <div>
       <h2>Welcome to ShoppingList</h2>
       <button onClick={handleClick}>New List</button>
+      <label>
+        Join List
+        <input type="text" placeholder="XXXX-XXXX-XXXX" />
+        <button>Join</button>
+      </label>
     </div>
   );
 };
