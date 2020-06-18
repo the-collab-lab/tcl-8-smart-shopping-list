b142251deba0962c9cd0558823ce24e7f9634e88
AliLynne <alilynne.thompson@gmail.com>
2020-06-18 16:16:15 -0700

    moved token logic to welcome component

--- src/App.js
+++ src/App.js
@@ -5,30 +5,15 @@ import { Route } from 'react-router-dom';
 import AddItem from './components/AddItem';
 import List from './components/List';
 import Welcome from './components/Welcome';
-import { Navigation } from './components/Navigation'
-
-import GetToken from './lib/token.js';
+import { Navigation } from './components/Navigation';
 
 function App() {
   const [token, setToken] = useState('');
   let history = useHistory();
   const push = history.push;
 
-  const handleClick = () => {
-    const newToken = GetToken();
-    saveToken(newToken);
-    setToken(newToken);
-    push('/list');
-  };
-
   const checkForToken = () => {
     return localStorage.getItem('userToken') ? true : false;
-  };
-
-  // token is saved locally, but not connected to firebase
-  const saveToken = token => {
-    localStorage.setItem('userToken', token);
-    console.log('token saved!');
   };
 
   useEffect(() => {
@@ -43,7 +28,7 @@ function App() {
       </header>
       <main>
         <Route exact path="/">
-          <Welcome handleClick={handleClick} />
+          <Welcome />
         </Route>
         <Route path="/list" component={List} />
         <Route path="/addItem" component={AddItem} />
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useHistory } from 'react';
 
-const Welcome = ({ handleClick }) => {
+import GetToken from '../lib/token.js';
+
+// need to create a form handling the tokens
+// https://rangle.io/blog/simplifying-controlled-inputs-with-hooks/
+
+const Welcome = () => {
+  const [token, setToken] = useState('');
+  //let history = useHistory();
+  //const push = history.push;
+
+  // token is saved locally, but not connected to firebase
+  const saveToken = token => {
+    localStorage.setItem('userToken', token);
+    console.log('token saved!');
+  };
+
+  const createNewList = () => {
+    const newToken = GetToken();
+    saveToken(newToken);
+    setToken(newToken);
+    //push('/list');
+  };
+
   return (
     <div>
       <h2>Welcome to ShoppingList</h2>
-      <button onClick={handleClick}>New List</button>
+      <button onClick={createNewList}>New List</button>
       <label>
         Join List
-        <input type="text" placeholder="XXXX-XXXX-XXXX" />
-        <button>Join</button>
+        <input type="text" placeholder="litton pawn tilth" />
+        <button onClick={e => saveToken(e.target.value)}>Join</button>
       </label>
     </div>
   );
