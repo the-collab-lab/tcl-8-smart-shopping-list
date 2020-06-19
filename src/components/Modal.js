f90b4ace072656a526faadd1da1224034914efeb
abidishajia <abidishajia@gmail.com>
2020-06-18 22:36:34 -0500

    updated modal styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,7 +5,11 @@ const Modal = props => {
   return (
     <div className="modal-wrapper">
       <div className="modal-backdrop" />
-      <div className="modal-box">Error Error</div>
+      <div className="modal-box">
+        <span onClick={() => props.setDisplay(false)}>X</span>
+        <h1> Error </h1>
+        <p> The item you've entered already exists. </p>
+      </div>
     </div>
   );
 };
