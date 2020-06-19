f05ca5958bd8ea3196cb19f1160f822d6b758d9e
lilyhoratio <lilyhzhou@gmail.com>
2020-06-19 12:59:10 -0700

    encapsulate label/button into form, added handleSubmit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,18 +7,27 @@ import GetToken from '../lib/token.js';
 
 const Welcome = () => {
   const [token, setToken] = useState('');
+
   //let history = useHistory();
   //const push = history.push;
 
   // token is saved locally, but not connected to firebase
-  const saveToken = token => {
-    localStorage.setItem('userToken', token);
-    console.log('token saved!');
+  // const saveToken = token => {
+  //   localStorage.setItem('userToken', token);
+  //   console.log('token saved!');
+  // };
+
+  const handleSubmitToken = e => {
+    e.preventDefault();
+
+    // check firebase for whether token exists
+    // if yes, save the token into local storage and push to `/list`
+    // if no, alert("invalid code, try again")
   };
 
   const createNewList = () => {
     const newToken = GetToken();
-    saveToken(newToken);
+    // saveToken(newToken);
     setToken(newToken);
     //push('/list');
   };
@@ -30,15 +39,19 @@ const Welcome = () => {
         <p>Create a new list: </p>
         <button onClick={createNewList}>New List</button>
       </div>
-      <div className="Welcome-JoinList">
+
+      <form className="Welcome-JoinList" onSubmit={handleSubmitToken}>
         <label>
           Join an existing list:
-          <input type="text" placeholder="litton pawn tilth" />
+          <input
+            type="text"
+            placeholder="litton pawn tilth"
+            onChange={e => setToken(e.target.value)}
+            value={token}
+          />
         </label>
-        <button onClick={e => saveToken(e.target.value)}>
-          Join Existing List
-        </button>
-      </div>
+        <button>Join Existing List</button>
+      </form>
     </div>
   );
 };
