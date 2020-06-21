907abcce17e74f64a93deef12ef8622531833913
AliLynne <alilynne.thompson@gmail.com>
2020-06-21 14:56:50 -0700

    fix circleCI error & remove unused vars

--- src/index.js
+++ src/index.js
@@ -8,18 +8,17 @@ import firebase from '@firebase/app';
 import '@firebase/firestore';
 import { FirestoreProvider } from 'react-firestore';
 
-var config = {
-  apiKey: 'AIzaSyA7inPAhY1nDezxRpyag6Ht7pMT7-vvKfo',
-  authDomain: 'temporary-a0728.firebaseapp.com',
-  databaseURL: 'https://temporary-a0728.firebaseio.com',
-  projectId: 'temporary-a0728',
-  storageBucket: 'temporary-a0728.appspot.com',
-  messagingSenderId: '743045505807',
-  appId: '1:743045505807:web:e75a64fc34ea3e19e9547a',
-  measurementId: 'G-TN3WQCF659',
+const tempConfig = {
+  apiKey: 'AIzaSyD4ldHcZXlbmNFVu6YtBskH-vQve2gRIn8',
+  authDomain: 'testproject-63838.firebaseapp.com',
+  databaseURL: 'https://testproject-63838.firebaseio.com',
+  projectId: 'testproject-63838',
+  storageBucket: 'testproject-63838.appspot.com',
+  messagingSenderId: '282733444013',
+  appId: '1:282733444013:web:900af5c5dd1cf525',
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(tempConfig);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
