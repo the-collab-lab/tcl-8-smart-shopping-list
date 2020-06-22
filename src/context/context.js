2b59aaf4991ff25f27b5b2eceb8de5cb51ab526e
github username <Lukabaramishvili@gmail.com>
2020-06-22 13:45:52 -0400

    remove componnentDidUpdate

--- src/context/context.js
+++ src/context/context.js
@@ -1,49 +1,40 @@
-import React, {Component} from 'react'
-import {db} from '../firebase.js'
+import React, { Component } from 'react';
+import { db } from '../firebase.js';
 
-const UserContext = React.createContext()
+const UserContext = React.createContext();
 
 class UserProvider extends Component {
   state = {
     shoppingList: [],
-  }
+  };
 
-  componentDidMount(){
-    db.collection('shoppingList').get()
+  componentDidMount() {
+    db.collection('shoppingList')
+      .get()
       .then(querySnapshot => {
         const data = querySnapshot.docs.map(doc => doc.data());
-        this.setState({shoppingList: data})
-      })
-  }
-
-  componentDidUpdate(){
-    db.collection('shoppingList').get()
-      .then(querySnapshot => {
-        const data = querySnapshot.docs.map(doc => doc.data());
-        this.setState({shoppingList: data})
-      })
+        this.setState({ shoppingList: data });
+      });
   }
 
   getShoppingList = () => this.state.shoppingList;
 
   render() {
-    const { children } = this.props
-    const { getShoppingList } = this
+    const { children } = this.props;
+    const { getShoppingList } = this;
 
     return (
       <UserContext.Provider
         value={{
-          getShoppingList
+          getShoppingList,
         }}
       >
         {children}
       </UserContext.Provider>
-    )
+    );
   }
 }
 
+export default UserContext;
 
-
-export default UserContext
-
-export { UserProvider }
+export { UserProvider };
