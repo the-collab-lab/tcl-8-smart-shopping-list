f04668bc50d60f11ea5e6c0e96ebd10ef9192cf2
AliLynne <alilynne.thompson@gmail.com>
2020-06-25 14:09:02 -0700

    add function to increment numberOfPurchases

--- src/components/List.js
+++ src/components/List.js
@@ -1,31 +1,50 @@
-import React, { Fragment } from 'react';
-import { FirestoreCollection } from 'react-firestore';
-import '../styles/List.css'
+import React from 'react';
+import { FirestoreCollection, withFirestore } from 'react-firestore';
+import '../styles/List.css';
 
-const List = () => {
+const List = ({ firestore }) => {
   const token = localStorage.getItem('userToken');
+
+  const handleChange = (e, item) => {
+    e.preventDefault();
+    const purchased = item.numberOfPurchases;
+    if (e.target.checked) {
+      firestore
+        .collection('shoppingList')
+        .doc(item.id)
+        .update({
+          numberOfPurchases: purchased + 1,
+        });
+    }
+  };
   return (
-      <FirestoreCollection
-        path="shoppingList"
-        filter={['token', '==', token]}
-        render={({ isLoading, data }) => {
-          return isLoading ? (
-            <p>loading...</p>
-          ) : (
-            <div className="list">
-              <ul style={{ listStyleType: 'none' }}>
-                {data.map(item => (
-                  <li key={item.id}>
-                    <input type="checkbox" defaultChecked="true"/>
-                    <span>{item.name} - next purchase in {item.nextPurchase} days </span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          );
-        }}
-      />
+    <FirestoreCollection
+      path="shoppingList"
+      filter={['token', '==', token]}
+      render={({ isLoading, data }) => {
+        return isLoading ? (
+          <p>loading...</p>
+        ) : (
+          <div className="list">
+            <ul style={{ listStyleType: 'none' }}>
+              {data.map(item => (
+                <li key={item.id}>
+                  <input
+                    type="checkbox"
+                    defaultChecked="true"
+                    onChange={e => handleChange(e, item)}
+                  />
+                  <span>
+                    {item.name} - next purchase in {item.nextPurchase} days{' '}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      }}
+    />
   );
 };
 
-export default List;
+export default withFirestore(List);
