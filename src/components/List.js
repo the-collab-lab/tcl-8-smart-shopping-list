3fcb81c322b95d5609567e1565639c079f1bc4ad
lilyhoratio <lilyhzhou@gmail.com>
2020-07-03 17:47:41 -0700

    renamed var to be more semantic

--- src/components/List.js
+++ src/components/List.js
@@ -3,8 +3,8 @@ import { useHistory } from 'react-router-dom';
 import { FirestoreCollection, withFirestore } from 'react-firestore';
 import Item from './Item';
 import '../styles/List.css';
-import calculateEstimate from '../lib/estimates'
-import secondsToDays from '../lib/secondsToDays'
+import calculateEstimate from '../lib/estimates';
+import secondsToDays from '../lib/secondsToDays';
 
 const List = ({ firestore }) => {
   const token = localStorage.getItem('userToken');
@@ -17,11 +17,18 @@ const List = ({ firestore }) => {
   };
 
   const handleChange = (e, item) => {
-    const daysInterval = secondsToDays((Date.now()/1000) - item.lastPurchasedDate['seconds'])
-    const estimate = calculateEstimate(item.nextPurchase, daysInterval, item.numberOfPurchases)
+    const daysInterval = secondsToDays(
+      Date.now() / 1000 - item.lastPurchasedDate['seconds'],
+    );
+    const weightedNextPurchaseEstimate = calculateEstimate(
+      item.nextPurchase,
+      daysInterval,
+      item.numberOfPurchases,
+    );
 
     const purchased = item.numberOfPurchases;
-    // TODO: Need some error handling here
+    // TODO: Need some error handling here to prevent user from unchecking and rechecking the box within 24
+    // hours of last checked date
     if (e.target.checked) {
       firestore
         .collection('shoppingList')
@@ -29,7 +36,7 @@ const List = ({ firestore }) => {
         .update({
           numberOfPurchases: purchased + 1,
           lastPurchasedDate: new Date(),
-          nextPurchase: estimate
+          nextPurchase: weightedNextPurchaseEstimate,
         });
     }
   };
