fd20d120cadb11e5e340f81b9a494eb41ff10f91
abidishajia <abidishajia@gmail.com>
2020-07-11 21:17:16 -0500

    fixed accessibility issu

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,20 +4,19 @@ import '../styles/Search.css';
 export default ({ handleInputChange, inputText, handleClearInput }) => {
   return (
     <div>
-      <label>
         <input
           className="search-input"
           onChange={handleInputChange}
           value={inputText}
           type="text"
           placeholder="search for your item..."
+          title="search"
         />
         {inputText.length > 0 && (
           <button className="clear-button" onClick={handleClearInput}>
             X
           </button>
         )}
-      </label>
     </div>
   );
 };
