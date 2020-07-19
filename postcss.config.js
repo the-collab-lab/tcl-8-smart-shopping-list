7de5de7e2ef87e8f0bd60fe2ce221c54beab5206
AliLynne <alilynne.thompson@gmail.com>
2020-07-19 09:28:32 -0700

    add explanatory comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
+// postcss is required for linting
+// and using autoprefixer to see which
+// css properties need to be prefixed
+
 const tailwindcss = require('tailwindcss');
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer')
-    ],
+  plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')],
 };
