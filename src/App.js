14accec14e00c107e7426f9cb406b05342713cbd
AliLynne <alilynne.thompson@gmail.com>
2020-07-22 13:24:19 -0700

    add background image

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import AddItem from './components/AddItem';
 import List from './components/List';
 import Welcome from './components/Welcome';
 import { Navigation } from './components/Navigation';
-
 import ListContextProvider from './context/ListContext';
+
+import lemons from './assets/images/lemons.jpg';
 
 function App() {
   const [token, setToken] = useState('');
@@ -23,14 +24,20 @@ function App() {
     setToken(localStorage.getItem('userToken'));
   }, [push]);
 
+  const background = {
+    backgroundImage: `url(${lemons})`,
+  };
+
   return (
     <ListContextProvider>
       <div
+        style={background}
         className="
-          border-b-8 border-teal-600 
-          flex-row max-w-4xl mx-auto 
+          border-b-8 border-teal-600
+          bg-center
+          bg-cover 
+          flex-row mx-auto 
           bg-gray-100
-          shadow-2xl 
           relative h-screen"
       >
         <header className="p-10">
@@ -45,6 +52,16 @@ function App() {
         </main>
 
         {token && <Navigation />}
+        <span>
+          Photo by{' '}
+          <a href="https://unsplash.com/@neryfabiola_?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+            Nery Montenegro
+          </a>{' '}
+          on{' '}
+          <a href="https://unsplash.com/s/photos/peach-color-food?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </span>
       </div>
     </ListContextProvider>
   );
