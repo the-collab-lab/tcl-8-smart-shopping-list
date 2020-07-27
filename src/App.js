a6a9b2050752642e4c7f4787030133490b706fa6
github username <Lukabaramishvili@gmail.com>
2020-07-27 17:40:14 -0400

    remove background img

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Welcome from './components/Welcome';
 import { Navigation } from './components/Navigation';
 import ListContextProvider from './context/ListContext';
 
-import lemons from './assets/images/lemons.jpg';
-
 function App() {
   const [token, setToken] = useState('');
   let history = useHistory();
@@ -24,23 +22,9 @@ function App() {
     setToken(localStorage.getItem('userToken'));
   }, [push]);
 
-  const background = {
-    backgroundImage: `url(${lemons})`,
-  };
-
   return (
     <ListContextProvider>
-      <div
-        style={background}
-        className="
-          border-b-8 border-teal-600
-          bg-center
-          bg-cover
-          flex 
-          flex-col mx-auto 
-          bg-gray-100
-          relative h-screen"
-      >
+      <div>
         <header className="p-4">
           <h1 className="text-4xl text-center text-teal-900">Smart Shopping</h1>
         </header>
@@ -51,20 +35,7 @@ function App() {
           <Route path="/list" component={List} />
           <Route path="/addItem" component={AddItem} />
         </main>
-
-        <footer className="self-end bg-cream p-2">
-          <span>
-            Photo by
-            <a href="https://unsplash.com/@neryfabiola_?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-              Nery Montenegro
-            </a>
-            on
-            <a href="https://unsplash.com/s/photos/peach-color-food?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-              Unsplash
-            </a>
-          </span>
-          {token && <Navigation />}
-        </footer>
+        {token && <Navigation />}
       </div>
     </ListContextProvider>
   );
